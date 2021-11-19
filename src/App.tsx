679157c6f8b3a3090eff1af3d23a4d5393d5c717
affandahar <daharaffan@gmail.com>
2021-11-19 08:42:03 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-import AddToList from './components/AddToList';
-import {RootState} from './redux/reducers'
-import { useSelector } from 'react-redux';
+
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
 
 
-export interface iState {
-  people : { 
-     name : string;
-     age : number;
-     url : string;
-     note? : string;
-  }[]
-}
-
-
 function App() {
-
-
-  
-
-
   return (
     <div className="App">
       <div className="container">
-
       <h1>Expense Tracker</h1>
       <Balance/>
       <IncomeExpense/>
